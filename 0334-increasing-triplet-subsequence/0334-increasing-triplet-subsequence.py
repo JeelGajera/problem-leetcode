6f9e1dda5e961509bda99ae3f36fd8df845e0991
Jeel Gajera <83470656+JeelGajera@users.noreply.github.com>
2024-01-05 13:38:02 +0530

    Time: 945 ms (33.15%), Space: 33.5 MB (6.29%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -0,0 +1,27 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        if not nums:
+            return 0
+
+        sub = []
+
+        def binary_search(sub, val):
+            lo, hi = 0, len(sub)-1
+            while lo <= hi:
+                mid = lo + (hi-lo)//2
+                if sub[mid] < val:
+                    lo = mid + 1
+                elif sub[mid] > val:
+                    hi = mid - 1
+                else:
+                    return mid
+            return lo
+
+        for x in nums:
+            i = binary_search(sub, x)
+            if i == len(sub):
+                sub.append(x)
+            else:
+                sub[i] = x
+        
+        return len(sub) >= 3
