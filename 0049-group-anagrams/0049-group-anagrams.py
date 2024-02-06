02cca2cad7427c9ac8c4b7f512f942ad6f8651f0
Jeel Gajera <83470656+JeelGajera@users.noreply.github.com>
2024-02-06 13:18:57 +0530

    Time: 86 ms (78.80%), Space: 20 MB (78.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,7 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        n = len(strs)
-        d = defaultdict(list)
-        for i in strs:
-            key = ''.join(sorted(i))
-            d[key].append(i)
-        return d.values()
-        
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = defaultdict(list)
+        for s in strs:
+            hash_map["".join(sorted(s))].append(s)
+
+        return hash_map.values()
