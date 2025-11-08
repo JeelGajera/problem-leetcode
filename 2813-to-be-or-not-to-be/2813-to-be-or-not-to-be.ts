c369e276bbaba036edfcc8a706cd8cd96981d4dd
Jeel Gajera <jeelgajera200@gmail.com>
2025-11-08 14:56:26 +0530

    Time: 48 ms (28.48%), Space: 55.6 MB (15.27%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.ts
@@ -0,0 +1,22 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: (a) => {
+            if (val === a) return true
+            throw Error("Not Equal")
+        },
+        notToBe: (b) => {
+            if (val !== b) return true
+            throw Error("Equal")
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
